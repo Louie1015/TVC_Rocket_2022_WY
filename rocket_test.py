6564197b66faf15b6ebc82960f11f2f30b758a54
Louie1015 <75915350+Louie1015@users.noreply.github.com>
2022-03-19 22:38:43 -0500

    Add files via upload

---
+++ rocket_test.py
@@ -0,0 +1,24 @@
+# this is a rocket flight simulation for model rockets
+
+# imports and libraries
+import numpy as np
+import matplotlib
+import turtle
+import time
+
+# params n stuff
+TIMER = 0
+
+
+class Simulation(object):
+    def _init_(self):
+        self.screen = turtle.screensize()
+        self.screen.setup(1280, 900)
+        self.marker = turtle.Turtle()
+        self.marker = 
+def main():
+    while(TIMER < 5):
+        sim = Simulation()
+        time.sleep(1)
+        timer += 1
+    main()
